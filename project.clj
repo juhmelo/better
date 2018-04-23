d52887c47463fc7a753fd49773a7adbdfe4a59d6
Juho Ylipoti <juho.ylipoti@zalando.de>
2018-04-23 09:54:24 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject better "0.1.0-SNAPSHOT"
+(defproject better "0.1.0"
   :description "Better things."
   :url "https://github.com/juhmelo/better"
   :license {:name "Eclipse Public License"
